c35137c439a591cfdbb390e9e3e960281983c090
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-02-06 15:58:05 +0300

    UI PhotosCollectionViewController

--- PhotoGallery/PhotosCollectionViewController.swift
+++ PhotoGallery/PhotosCollectionViewController.swift
@@ -9,9 +9,76 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    private lazy var addBurButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonTapped))
+    }()
+    
+    private lazy var actionBarButtonItem = {
+       return UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(actionBarButtonTapped))
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .orange
+        setupCollectionView()
+        setupNavigationBar()
+        setupSearchBar()
     }
     
+    //MARK: - NavigationItems action
+    
+    @objc private func addBarButtonTapped() {
+        print(#function)
+    }
+    
+    @objc private func actionBarButtonTapped() {
+        print(#function)
+    }
+    
+    //MARK: - Setup UI elements
+    
+    private func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
+    }
+    private func setupNavigationBar() {
+        let titleLabel = UILabel()
+        titleLabel.text = "PHOTOS"
+        titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        titleLabel.textColor = #colorLiteral(red: 0.5019607843, green: 0.4980392157, blue: 0.4980392157, alpha: 1)
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+        navigationItem.rightBarButtonItems = [actionBarButtonItem, addBurButtonItem]
+    }
+    
+    private func setupSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+        
+    }
+    
+    //MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+}
+
+//MARK: - UISearchBarDelegate
+
+extension PhotosCollectionViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
 }
