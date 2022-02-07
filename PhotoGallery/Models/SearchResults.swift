0ecb9fa634f301730a001ec7f03253518f69b62a
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-02-07 18:14:16 +0300

    model SearchResults

---
+++ PhotoGallery/Models/SearchResults.swift
@@ -0,0 +1,26 @@
+//
+//  SearchResults.swift
+//  PhotoGallery
+//
+//  Created by MacBookPro on 07.02.2022.
+//
+
+import Foundation
+
+struct SearchResults: Decodable {
+    let total: Int
+    let results: [UnsplashPhoto]
+}
+struct UnsplashPhoto: Decodable {
+    let width: Int
+    let height: Int
+    let urls: [URLKing.RawValue: String]
+    
+    enum URLKing: String {
+        case raw
+        case full
+        case regular
+        case small
+        case thumb
+    }
+}
---
+++ PhotoGallery/Service/NetworkDataFetcher.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkDataFetcher.swift
+//  PhotoGallery
+//
+//  Created by MacBookPro on 07.02.2022.
+//
+
+import Foundation
+
+class NetworkDataFetcher {
+    func getImages(searchTerm: String, completion: [])
+}
