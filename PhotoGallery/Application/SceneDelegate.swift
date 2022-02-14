07c41659f81050b49bdd9a51764caac2340396eb
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-02-14 20:59:42 +0300

    ref

--- PhotoGallery/Application/SceneDelegate.swift
+++ PhotoGallery/Application/SceneDelegate.swift
@@ -19,7 +19,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.rootViewController = MainTabBarController()
         window.makeKeyAndVisible()
         self.window = window
-        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -49,6 +48,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
 }
 
--- PhotoGallery/Models/SearchResults.swift
+++ PhotoGallery/Models/SearchResults.swift
@@ -11,6 +11,7 @@ struct SearchResults: Decodable {
     let total: Int
     let results: [UnsplashPhoto]
 }
+
 struct UnsplashPhoto: Decodable {
     let width: Int
     let height: Int
